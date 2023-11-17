0509a2b52c105ba01ada663fae7e6cd20e51ae3e
adityadewhy <130918775+adityadewhy@users.noreply.github.com>
2023-11-17 20:59:17 +0000

    string concatenation done

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.body.appendChild(paragraphElement);
 // Question-7:    WAP to input the distance between two cities (in km) , convert and print this distance in meter, feet, inches, and centimeter.
 
 // const a = prompt("enter distance in km")
+// make sure to convert a to integer (it is orignally string)
 // const paragraphElement = document.createElement('p');
 // document.body.appendChild(paragraphElement);
 
@@ -13,30 +14,36 @@ document.body.appendChild(paragraphElement);
 
 
 //switch case ques
-const dayNumber = prompt('enter number from 1 to 7');
+// const dayNumber = prompt('enter number from 1 to 7');
 
-switch (parseInt(dayNumber)) {
-  case 1:
-    paragraphElement.innerHTML = ("Monday");
-    break;
-  case 2:
-      paragraphElement.innerHTML = ("Tuesday");
-    break;
-  case 3:
-      paragraphElement.innerHTML = ("Wednesday");
-    break;
-  case 4:
-      paragraphElement.innerHTML = ("Thursday");
-    break;
-  case 5:
-      paragraphElement.innerHTML = ("Friday");
-    break;
-  case 6:
-      paragraphElement.innerHTML = ("Saturday");
-    break;
-  case 7:
-    paragraphElement.innerHTML = ("Sunday");
-    break;
-  default:
-      paragraphElement.innerHTML = ("Invalid day number");
-}
+// switch (parseInt(dayNumber)) {
+//   case 1:
+//     paragraphElement.innerHTML = ("Monday");
+//     break;
+//   case 2:
+//       paragraphElement.innerHTML = ("Tuesday");
+//     break;
+//   case 3:
+//       paragraphElement.innerHTML = ("Wednesday");
+//     break;
+//   case 4:
+//       paragraphElement.innerHTML = ("Thursday");
+//     break;
+//   case 5:
+//       paragraphElement.innerHTML = ("Friday");
+//     break;
+//   case 6:
+//       paragraphElement.innerHTML = ("Saturday");
+//     break;
+//   case 7:
+//     paragraphElement.innerHTML = ("Sunday");
+//     break;
+//   default:
+//       paragraphElement.innerHTML = ("Invalid day number");
+// }
+
+
+// string concatenation
+const dayNumber1 = prompt('enter number from 1 to 7 first time: ');
+const dayNumber2 = prompt('enter number from 1 to 7 second time: ');
+paragraphElement.innerHTML = `concatenated string ${dayNumber1 + dayNumber2}`;
