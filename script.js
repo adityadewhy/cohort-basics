cd0200ae7c29d10952176bd054ff4eb8634c163d
adityadewhy <130918775+adityadewhy@users.noreply.github.com>
2023-11-17 20:53:28 +0000

    switch case done

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 // https://gist.github.com/9520prashant/55f05f84ecf33c1edccaaf2eccfe256c
 
-// Question-7:    WAP to input the distance between two cities (in km) , convert and print this distance in meter, feet, inches, and centimeter.
-
-const a = prompt("enter distance in km")
 const paragraphElement = document.createElement('p');
 document.body.appendChild(paragraphElement);
 
-paragraphElement.innerHTML += `in km: ${a*1000}, in cm: ${a*100000} `;
+// Question-7:    WAP to input the distance between two cities (in km) , convert and print this distance in meter, feet, inches, and centimeter.
+
+// const a = prompt("enter distance in km")
+// const paragraphElement = document.createElement('p');
+// document.body.appendChild(paragraphElement);
+
+// paragraphElement.innerHTML += `in km: ${a*1000}, in cm: ${a*100000} `;
+
+
+//switch case ques
+const dayNumber = prompt('enter number from 1 to 7');
+
+switch (parseInt(dayNumber)) {
+  case 1:
+    paragraphElement.innerHTML = ("Monday");
+    break;
+  case 2:
+      paragraphElement.innerHTML = ("Tuesday");
+    break;
+  case 3:
+      paragraphElement.innerHTML = ("Wednesday");
+    break;
+  case 4:
+      paragraphElement.innerHTML = ("Thursday");
+    break;
+  case 5:
+      paragraphElement.innerHTML = ("Friday");
+    break;
+  case 6:
+      paragraphElement.innerHTML = ("Saturday");
+    break;
+  case 7:
+    paragraphElement.innerHTML = ("Sunday");
+    break;
+  default:
+      paragraphElement.innerHTML = ("Invalid day number");
+}
