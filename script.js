5357418e40544fcabaa200d0de25dacd4f31b5ab
adityadewhy <130918775+adityadewhy@users.noreply.github.com>
2023-11-17 13:37:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// https://gist.github.com/9520prashant/55f05f84ecf33c1edccaaf2eccfe256c
+
+// Question-7:    WAP to input the distance between two cities (in km) , convert and print this distance in meter, feet, inches, and centimeter.
+
+const a = prompt("enter value in km")
+
+const b = a * 1000
+console.log(b)
